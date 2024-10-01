051996330f8a5859aa98334e3e8d3142adb4867b
Dinis Cruz <dinis.cruz@owasp.org>
2024-10-01 17:13:19 +0100

    refactored services to flows

--- tests/unit/utils/test_Version.py
+++ tests/unit/utils/test_Version.py
@@ -2,8 +2,8 @@ from unittest import TestCase
 
 from osbot_utils.utils.Files import file_name, folder_name, parent_folder
 
-import osbot_serverless_services
-from osbot_serverless_services.utils.Version import Version, version
+import osbot_serverless_flows
+from osbot_serverless_flows.utils.Version import Version, version
 
 
 class test_Version(TestCase):
@@ -13,11 +13,11 @@ class test_Version(TestCase):
         cls.version = Version()
 
     def test_path_code_root(self):
-        assert self.version.path_code_root() == osbot_serverless_services.path
+        assert self.version.path_code_root() == osbot_serverless_flows.path
 
     def test_path_version_file(self):
         with self.version as _:
-            assert parent_folder(_.path_version_file()) == osbot_serverless_services.path
+            assert parent_folder(_.path_version_file()) == osbot_serverless_flows.path
             assert file_name    (_.path_version_file()) == 'version'
 
     def test_value(self):

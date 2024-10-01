9f1f84260516c1d448dce4c5b603044f6ed545d0
Dinis Cruz <dinis.cruz@owasp.org>
2024-10-01 22:30:28 +0100

    added test using requests

--- tests/integration/test__local_docker_image.py
+++ tests/integration/test__local_docker_image.py
@@ -1,5 +1,8 @@
 from unittest import TestCase
 
+import pytest
+import requests
+
 from osbot_aws.aws.lambda_.Lambda                   import Lambda
 from osbot_aws.aws.session.Session__Kwargs__Lambda  import Session__Kwargs__Lambda
 
@@ -7,10 +10,26 @@ from osbot_aws.aws.session.Session__Kwargs__Lambda  import Session__Kwargs__Lamb
 class test__local_docker_image(TestCase):
     @classmethod
     def setUpClass(cls):
-        endpoint_url      = 'http://localhost:5002'
+        cls.endpoint_url  = 'http://localhost:5002'
         cls.function_name = 'function'
-        cls.session_kwargs__lambda = Session__Kwargs__Lambda(endpoint_url=endpoint_url).__locals__()
+        cls.session_kwargs__lambda = Session__Kwargs__Lambda(endpoint_url=cls.endpoint_url).__locals__()
 
+    @pytest.mark.skip()
     def test__invoke_lambda(self):
         with Lambda(name=self.function_name, session_kwargs__lambda=self.session_kwargs__lambda) as _:
             assert _.invoke({}) == {'body': 'Hello from Docker Lambda!', 'statusCode': 200}
+
+    def test__invoke_lambda__using_requests(self):
+        # The local endpoint to invoke the Lambda function using HTTP
+        invoke_url = f"{self.endpoint_url}/2015-03-31/functions/{self.function_name}/invocations"
+
+        # Define the payload you want to send to the Lambda function
+        payload = {}
+
+        # Send a POST request to the local Lambda endpoint
+        response = requests.post(invoke_url, json=payload)
+
+        # Check the response from the Lambda function
+        assert response.status_code == 200
+        assert response.json() == {'body': 'Hello from Docker Lambda!', 'statusCode': 200}
+

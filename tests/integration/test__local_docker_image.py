6b8c9f7431bf5ea88b3438d17497ca4714c99bbd
Dinis Cruz <dinis.cruz@owasp.org>
2024-10-01 22:55:46 +0100

    added use of Temp__Random__AWS_Credentials

--- tests/integration/test__local_docker_image.py
+++ tests/integration/test__local_docker_image.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 
 import pytest
 import requests
+from osbot_aws.testing.Temp__Random__AWS_Credentials import Temp__Random__AWS_Credentials
 
 from osbot_aws.aws.lambda_.Lambda                   import Lambda
 from osbot_aws.aws.session.Session__Kwargs__Lambda  import Session__Kwargs__Lambda
@@ -15,8 +16,9 @@ class test__local_docker_image(TestCase):
         cls.session_kwargs__lambda = Session__Kwargs__Lambda(endpoint_url=cls.endpoint_url).__locals__()
 
     def test__invoke_lambda(self):
-        with Lambda(name=self.function_name, session_kwargs__lambda=self.session_kwargs__lambda) as _:
-            assert _.invoke({}) == {'body': 'Hello from Docker Lambda!', 'statusCode': 200}
+        with Temp__Random__AWS_Credentials():
+            with Lambda(name=self.function_name, session_kwargs__lambda=self.session_kwargs__lambda) as _:
+                assert _.invoke({}) == {'body': 'Hello from Docker Lambda!', 'statusCode': 200}
 
     def test__invoke_lambda__using_requests(self):
         # The local endpoint to invoke the Lambda function using HTTP

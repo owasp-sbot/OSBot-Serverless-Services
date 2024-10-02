0b30f963912811f778b0e04c8f7ffb9092b6a740
Dinis Cruz <dinis.cruz@owasp.org>
2024-10-02 11:58:53 +0100

    wired qa tests in the correct step of the CI Pipeline

--- tests/qa/test__live_lambda_server.py
+++ tests/qa/test__live_lambda_server.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+import requests
 from osbot_utils.utils.Dev import pprint
 from osbot_utils.utils.Http import GET
 
@@ -7,6 +8,7 @@ from osbot_utils.utils.Http import GET
 class test__live_lambda_server(TestCase):
 
     def test__invoke_url(self):
-        live_url = 'https://a4sbatx723cfs7tw3kbiifv37q0evpcp.lambda-url.eu-west-1.on.aws'
-
-        pprint(GET(live_url))
+        function_url = 'https://a4sbatx723cfs7tw3kbiifv37q0evpcp.lambda-url.eu-west-1.on.aws'
+        response     = requests.get(function_url)
+        assert response.status_code == 200
+        assert response.text        == 'Hello from Docker Lambda!'

6993af4877474396d9f05abc33740434249922f7
Dinis Cruz <dinis.cruz@owasp.org>
2024-10-02 17:19:55 +0100

    fixed QA test and added ability to test QA files locally

--- osbot_serverless_flows/lambdas/handler.py
+++ osbot_serverless_flows/lambdas/handler.py
@@ -16,3 +16,7 @@ async def ping():
 
 
 run = Mangum(app)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
--- tests/qa/test__live_lambda_server.py
+++ tests/qa/test__live_lambda_server.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 import requests
 from osbot_utils.utils.Dev import pprint
-from osbot_utils.utils.Env import get_env
+from osbot_utils.utils.Env import get_env, load_dotenv
 from osbot_utils.utils.Http import GET
 
 from osbot_serverless_flows.utils.Version import version__osbot_serverless_flows
@@ -12,19 +12,19 @@ ENDPOINT_URL__QA_LAMBDA = 'https://a4sbatx723cfs7tw3kbiifv37q0evpcp.lambda-url.e
 class test__live_lambda_server(TestCase):
 
     def setUp(self):
+        load_dotenv()
         self.endpoint_url = get_env('ENDPOINT_URL__QA_LAMBDA', ENDPOINT_URL__QA_LAMBDA)
-        pprint(self.endpoint_url)
 
 
     def test__ping(self):
         response = requests.get(f"{self.endpoint_url}/ping")
         assert response.status_code == 200
-        assert response.json() == {"message": "pong"}
+        assert response.json() == {'pong': '42'}
 
     def test__docs(self):
         response = requests.get(f"{self.endpoint_url}/docs")
         assert response.status_code == 200
-        assert "Swagger UI" in response.text  # Checks for the presence of Swagger UI in the docs
+        assert "Swagger UI" in response.text
 
     def test__openapi(self):
         response = requests.get(f"{self.endpoint_url}/openapi.json")

8ed1aec9647415018cb6b6f47f48ad38e0c2d2e9
Dinis Cruz <dinis.cruz@owasp.org>
2024-10-02 13:28:03 +0100

    major refactoring and wire of lambda handler from the main code base

--- tests/qa/test__live_lambda_server.py
+++ tests/qa/test__live_lambda_server.py
@@ -4,11 +4,14 @@ import requests
 from osbot_utils.utils.Dev import pprint
 from osbot_utils.utils.Http import GET
 
+from osbot_serverless_flows.utils.Version import version__osbot_serverless_flows
+
 
 class test__live_lambda_server(TestCase):
 
     def test__invoke_url(self):
-        function_url = 'https://a4sbatx723cfs7tw3kbiifv37q0evpcp.lambda-url.eu-west-1.on.aws'
-        response     = requests.get(function_url)
+        expected_message = f'Hello from main code Lambda! | version: {version__osbot_serverless_flows}'
+        function_url     = 'https://a4sbatx723cfs7tw3kbiifv37q0evpcp.lambda-url.eu-west-1.on.aws'
+        response         = requests.get(function_url)
         assert response.status_code == 200
-        assert response.text        == 'Hello from Docker Lambda!'
+        assert response.text        == expected_message

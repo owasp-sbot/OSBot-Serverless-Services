f47e79c2ce743e2427f4005be6c85f4abe707dc6
Dinis Cruz <dinis.cruz@owasp.org>
2024-10-04 13:14:04 +0100

    first working MVP of Flow__Playwright__Get_Page_Html

--- tests/integration/flows/browser_based/test_Flow__Playwright__Get_Page_Html.py
+++ tests/integration/flows/browser_based/test_Flow__Playwright__Get_Page_Html.py
@@ -1,5 +1,18 @@
 from unittest import TestCase
 
+from osbot_utils.utils.Dev import pprint
+
+from osbot_serverless_flows.flows.browser_based.Flow__Playwright__Get_Page_Html import Flow__Playwright__Get_Page_Html
+
 
 class test_Flow__Playwright__Get_Page_Html(TestCase):
-    pass
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.flow__get_page_html = Flow__Playwright__Get_Page_Html()
+
+    def test_run(self):
+        flow_data = self.flow__get_page_html.run()
+        page_content = flow_data.get('page_content')
+        assert "<title>Google</title>" in page_content
+        assert len(page_content) > 10000

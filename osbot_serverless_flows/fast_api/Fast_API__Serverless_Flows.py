b39dd35fa7ae49ebcd9e915de8fea1b948677050
Dinis Cruz <dinis.cruz@owasp.org>
2024-10-04 16:46:43 +0100

    added ensure_browser_is_installed to setup

--- osbot_serverless_flows/fast_api/Fast_API__Serverless_Flows.py
+++ osbot_serverless_flows/fast_api/Fast_API__Serverless_Flows.py
@@ -7,6 +7,20 @@ from osbot_serverless_flows.fast_api.routes.Routes__Browser import Routes__Brows
 
 class Fast_API__Serverless_Flows(Fast_API):
 
+    def ensure_browser_is_installed(self):
+        from osbot_serverless_flows.playwright.Playwright__Serverless import Playwright__Serverless
+        playwright_browser = Playwright__Serverless()
+        result = playwright_browser.browser__install()
+        print(f"*****************************************")
+        print(f"*****    Chrome installed:  { result }    *****")
+        print(f"*****************************************")
+
+    def setup(self):
+        super().setup()
+        self.ensure_browser_is_installed()
+        return self
+
+
     def setup_routes(self):
         self.add_routes(Routes__Info)
         self.add_routes(Routes__Debug)
